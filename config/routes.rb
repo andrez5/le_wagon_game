789a91c6f641ec3bfb24cdfd796b20ee7eae1317
Michel Moutinho <michelmoutinhofr1@gmail.com>
2022-11-29 11:56:36 -0300

    choices controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'choices/new'
+  get 'choices/create'
+  get 'choices/index'
+  get 'choices/show'
+  get 'choices/destroy'
+  get 'choices/edit'
+  get 'choices/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :choices
   # Defines the root path route ("/")
   # root "articles#index"
 end
