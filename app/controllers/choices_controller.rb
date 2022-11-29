add18d8b7bc5066e1bf1b670511154be9eea9446
Michel Moutinho <michelmoutinhofr1@gmail.com>
2022-11-29 15:02:05 -0300

    choices form alteração no controler

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,14 +1,4 @@
 class ChoicesController < ApplicationController
-  def new
-    @choice = Choice.new
-  end
-
-  def create
-    @choice = Choice.new(choice_params)
-    @choice.save
-    redirect_to choice_path(@choice)
-  end
-
   def index
     @choices = Choice.all
   end
@@ -17,10 +7,23 @@ class ChoicesController < ApplicationController
     @choice = Choice.find(params[:id])
   end
 
-  def destroy
-    @choice = Choice.find(params[:id])
-    @choice.destroy
-    redirect_to choices_path, status: :see_other
+  def new
+    @scene = Scene.find(params[:scene_id])
+    @choice = Choice.new
+  end
+
+  def create
+    @scene = Scene.find(params[:scene_id])
+    @choice = Choice.new(choice_params)
+    @choice.scene = @scene
+    # @choice.user = current_user
+    if @choice.save
+      # @scene.available -= @choice.quantity
+      @scene.save
+      redirect_to scene_choice_path(@scene, @choice), notice: "ok"
+    else
+      render 'scenes/show', status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -28,14 +31,22 @@ class ChoicesController < ApplicationController
   end
 
   def update
+    if @choice.update(choice_params)
+      redirect_to @choice, notice: "choice was successfully update."
+    else
+      render :edit, status: :unproccessable_entity
+    end
+  end
+
+  def destroy
     @choice = Choice.find(params[:id])
-    @choice.update(choice_params)
-    redirect_to choice_path(@choice)
+    @choice.destroy
+    redirect_to choices_path, status: :see_other
   end
 
   private
 
   def choice_params
-    params.require(:choice).permit(:name, :address, :rating)
+    params.require(:choice).permit(:next_scene_id, :scene_id)
   end
 end
