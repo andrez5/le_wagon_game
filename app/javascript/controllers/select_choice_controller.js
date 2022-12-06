db8595ea1ab2d85a5f85dc714112536517ff3410
Sergio Kummer <sergiokummer96@gmail.com>
2022-12-06 12:25:23 -0300

    js feito

--- app/javascript/controllers/select_choice_controller.js
+++ app/javascript/controllers/select_choice_controller.js
@@ -2,15 +2,41 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="select-choice"
 export default class extends Controller {
+  static targets = ["choice"]
+
   connect() {
     console.log("opa iai vei")
+    console.log(this.choiceTargets[0])
+    console.log(this.choiceTargets.length)
+    this.choiceTargets[0].classList.add("bg-choice")
   }
 
-  const moveChoice = (event) => {
-    if (event.key === 38) {
-      moveForward(1);
-    } else if (event.key === 40) {
-      moveForward(2);
+  choose(event) {
+    this.chosen = this.choiceTargets.find(choice => choice.classList.contains("bg-choice"))
+    if (event.key === "ArrowUp") {
+      if (this.chosen === this.choiceTargets[0]) {
+        this.chosen.classList.remove("bg-choice")
+        this.choiceTargets[this.choiceTargets.length - 1].classList.add("bg-choice")
+      } else {
+        this.chosen.classList.remove("bg-choice")
+        this.chosen.previousElementSibling.classList.add("bg-choice")
+      }
     }
-  };
+    if (event.key === "ArrowDown") {
+      if (this.chosen === this.choiceTargets[this.choiceTargets.length - 1]) {
+        this.chosen.classList.remove("bg-choice")
+        this.choiceTargets[0].classList.add("bg-choice")
+      } else {
+        this.chosen.classList.remove("bg-choice")
+        this.chosen.nextElementSibling.classList.add("bg-choice")
+      }
+    }
+  }
+
+  selectChoice(event) {
+    this.chosen = this.choiceTargets.find(choice => choice.classList.contains("bg-choice"))
+    if (event.key === " " || event.key === "Enter") {
+      this.chosen.click()
+    }
+  }
 }
