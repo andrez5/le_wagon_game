72ca06fcc6e243052104a383208ff68e571d2a11
henrique marques <ricaooliveira1@gmail.com>
2022-12-06 16:40:50 -0300

    form pc

--- app/models/pc.rb
+++ app/models/pc.rb
@@ -3,5 +3,7 @@ class Pc < ApplicationRecord
   belongs_to :scene
   validates :pc_name, presence: true
   validates :gender, presence: true
-  validates :age, presence: true
+  validates :age, numericality: { greater_than_or_equal_to: 1 }, presence: true
+  GENDERS = ["Homem", "Mulher", "Outros"]
+  # validates :pc_name, presence: { error_message: "Email can't be blank" }
 end
