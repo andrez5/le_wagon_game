85a5fbd04de6e2a0caf33c4e718fbd9ee8331bdf
Sergio Kummer <sergiokummer96@gmail.com>
2022-12-08 10:02:23 -0300

    tirando o lixo

--- app/controllers/pcs_controller.rb
+++ app/controllers/pcs_controller.rb
@@ -1,25 +1,5 @@
 class PcsController < ApplicationController
 
-  def index
-    @pcs = Pc.all
-  end
-
-  def show
-    @pc = Pc.find(params[:id])
-  end
-
-  def edit
-    @pc = Pc.find(params[:id])
-  end
-
-  def update
-    if @pc.update(pc_params)
-      redirect_to @pc, notice: "pc was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   def new
     @pc = Pc.new
   end
@@ -35,11 +15,7 @@ class PcsController < ApplicationController
     end
   end
 
-  def destroy
-    @pc = Pc.find(params[:id])
-    @pc.destroy
-    redirect_to pcs_path, status: :see_other
-  end
+  private
 
   def pc_params
     params.require(:pc).permit(:pc_name, :gender, :age)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  resources :scenes do
+  resources :scenes, path: 'game', only: [:show, :new, :create] do
     resources :choices
   end
 
   root to: "pages#home"
 
-  resources :pcs
+  resources :pcs, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
 end
